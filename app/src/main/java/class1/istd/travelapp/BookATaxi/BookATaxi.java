8f4894954f5a334ad02c181ab4a88c8f6c8bafde
ayrtx <jesandry@gmail.com>
2016-11-17 21:47:32 +0800

    added taxi info

---
+++ app/src/main/java/class1/istd/travelapp/BookATaxi/BookATaxi.java
@@ -0,0 +1,14 @@
+package com.example.student.bookataxi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BookATaxi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bookataxi);
+
+    }
+}
