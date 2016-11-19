cb00a7a454b8e64e07abfc5aae26e43923803568
privatejw <privatejw@gmail.com>
2016-11-20 00:00:46 +0800

    fixed bug in MapsActivity

--- app/src/main/java/class1/istd/travelapp/Location/MapActivity.java
+++ app/src/main/java/class1/istd/travelapp/Location/MapActivity.java
@@ -99,6 +99,7 @@ public class MapActivity extends BaseActivity implements OnMapReadyCallback {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
+        super.onOptionsItemSelected(item);
         int id = item.getItemId();
 
         switch (id){
